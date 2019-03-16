76ebf52e025bb4a5039ab2cc12adfd6d51900e6e
Ajmal Ali <ajmalsyedali@gmail.com>
2019-03-16 23:15:02 +0300

    Admin view

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,27 +1,14 @@
 const init = () => {
-    let signin = document.getElementById('signin-card');
-    let signup = document.getElementById('signup-card');
-    let signinForm = document.getElementById('signin-form');
-    let signupForm = document.getElementById('signup-form');
 
-    // Disable the signin form
-    signin.style.display = 'none';
-    signinForm.disabled = true;
-
-    // Event handler for signup radio
-    document.getElementById('tab-1').addEventListener('click', function () {
-        signin.style.display = 'none';
-        signinForm.disabled = true;
-        signup.style.display = 'block';
-        signupForm.disabled = false;
-    });
-
-    // Event handler for signin radio
-    document.getElementById('tab-2').addEventListener('click', function () {
-        signup.style.display = 'none';
-        signupForm.disabled = true;
-        signin.style.display = 'block';
-        signinForm.disabled = false;
+    document.getElementById('auth-form').addEventListener('submit', function () {
+       let formData = new FormData(this);
+       if (formData.get('id') === "admin") {
+           this.action = 'admin/requests.html';
+       } else if (formData.get('id') === "user") {
+           this.action = 'user-dashboard.html';
+       } else if (formData.get('id') === "staff") {
+           this.action = 'staff-dashboard.html';
+       }
     });
 };
 
